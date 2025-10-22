552bd88a95133e3a6bc2e21a1b86feed439d1f74
Danya2744 <danya.vybornov.94@bk.ru>
2025-10-22 19:58:44 +0300

    Добавлена страница для добавления нового пользователя и реализован функционал для нее.

--- Pages/AddUserPage.xaml.cs
+++ Pages/AddUserPage.xaml.cs
@@ -15,14 +15,65 @@ using System.Windows.Shapes;
 
 namespace _422_Vybornov.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для AddUserPage.xaml
-    /// </summary>
     public partial class AddUserPage : Page
     {
-        public AddUserPage()
+        private User _currentUser = new User();
+        public AddUserPage(User selectedUser)
         {
             InitializeComponent();
+
+            if (selectedUser != null)
+                _currentUser = selectedUser;
+
+            DataContext = _currentUser;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentUser.Login))
+                errors.AppendLine("Укажите логин!");
+
+            if (string.IsNullOrWhiteSpace(_currentUser.Password))
+                errors.AppendLine("Укажите пароль!");
+
+            if ((_currentUser.Role == null) || (cmbRole.Text == ""))
+                errors.AppendLine("Выберите роль!");
+            else
+                _currentUser.Role = cmbRole.Text;
+
+            if (string.IsNullOrWhiteSpace(_currentUser.FIO))
+                errors.AppendLine("Укажите ФИО");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentUser.ID == 0)
+                Vybornov_DB_PaymentEntities1.GetContext().User.Add(_currentUser);
+
+            try
+            {
+                Vybornov_DB_PaymentEntities1.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно сохранены!");
+                NavigationService?.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void ButtonClean_Click(object sender, RoutedEventArgs e)
+        {
+            TBLogin.Text = "";
+            TBPass.Text = "";
+            cmbRole.SelectedIndex = -1;
+            TBFio.Text = "";
+            TBPhoto.Text = "";
         }
     }
 }
