75a37978896efff909d09715294ef91762e64af1
Danya2744 <danya.vybornov.94@bk.ru>
2025-10-22 18:57:57 +0300

    Полностью реализована страница авторизации

--- Pages/AuthPage.xaml.cs
+++ Pages/AuthPage.xaml.cs
@@ -34,7 +34,7 @@ namespace _422_Vybornov.Pages
                 return;
             }
 
-            string hashedPassword = GetHash(PasswordBox.Password);
+            string hashedPassword = GetHash(PasswordBox.Password); 
 
             using (var db = new Vybornov_DB_PaymentEntities1())
             {
@@ -47,15 +47,14 @@ namespace _422_Vybornov.Pages
                     MessageBox.Show("Пользователь с такими данными не найден!");
                     failedAttempts++;
 
-                    // Здесь нужно добавить логику для капчи
-                    // if (failedAttempts >= 3)
-                    // {
-                    //     if (captcha.Visibility != Visibility.Visible)
-                    //     {
-                    //         CaptchaSwitch();
-                    //     }
-                    //     CaptchaChange();
-                    // }
+                    if (failedAttempts >= 3)
+                    {
+                        if (captcha.Visibility != Visibility.Visible)
+                        {
+                            CaptchaSwitch();
+                        }
+                        CaptchaChange();
+                    }
                     return;
                 }
                 else
@@ -66,40 +65,113 @@ namespace _422_Vybornov.Pages
                     switch (user.Role)
                     {
                         //case "user":
-                        //    navigationservice?.navigate(new userpage());
+                        //    NavigationService?.Navigate(new UserPage());
                         //    break;
                         //case "admin":
-                        //    navigationservice?.navigate(new adminpage());
+                        //    NavigationService?.Navigate(new AdminPage());
                         //    break;
-                        //default:
-                        //    messagebox.show("неизвестная роль пользователя");
-                        //    break;
+                        default:
+                            MessageBox.Show("Неизвестная роль пользователя");
+                            break;
                     }
                 }
             }
         }
 
+        public void CaptchaSwitch()
+        {
+            switch (captcha.Visibility)
+            {
+                case Visibility.Visible:
+                    TextBoxLogin.Clear();
+                    PasswordBox.Clear();
+                    captcha.Visibility = Visibility.Hidden;
+                    captchaInput.Visibility = Visibility.Hidden;
+                    labelCaptcha.Visibility = Visibility.Hidden;
+                    submitCaptcha.Visibility = Visibility.Hidden;
+
+                    labelPass.Visibility = Visibility.Visible;
+                    TextBoxLogin.Visibility = Visibility.Visible;
+                    PasswordBox.Visibility = Visibility.Visible;
+                    ButtonChangePassword.Visibility = Visibility.Visible;
+                    ButtonEnter.Visibility = Visibility.Visible;
+                    ButtonReg.Visibility = Visibility.Visible;
+                    return;
+
+                case Visibility.Hidden:
+                    captcha.Visibility = Visibility.Visible;
+                    captchaInput.Visibility = Visibility.Visible;
+                    labelCaptcha.Visibility = Visibility.Visible;
+                    submitCaptcha.Visibility = Visibility.Visible;
+
+                    labelPass.Visibility = Visibility.Hidden;
+                    TextBoxLogin.Visibility = Visibility.Hidden;
+                    PasswordBox.Visibility = Visibility.Hidden;
+                    ButtonChangePassword.Visibility = Visibility.Hidden;
+                    ButtonEnter.Visibility = Visibility.Hidden;
+                    ButtonReg.Visibility = Visibility.Hidden;
+                    return;
+            }
+        }
+        public void CaptchaChange()
+        {
+            String allowchar = " ";
+            allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
+            allowchar += "1,2,3,4,5,6,7,8,9,0";
+
+            char[] a = { ',' };
+            String[] ar = allowchar.Split(a);
+            String pwd = "";
+            string temp = "";
+            Random r = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                temp = ar[(r.Next(0, ar.Length))];
+                pwd += temp;
+            }
+            captcha.Text = pwd;
+        }
+
+        private void submitCaptcha_Click(object sender, RoutedEventArgs e)
+        {
+            if (captchaInput.Text != captcha.Text)
+            {
+                MessageBox.Show("Неверно введена капча", "Ошибка");
+                CaptchaChange();
+            }
+            else
+            {
+                MessageBox.Show("Капча введена успешно, можете продолжить авторизацию", "Успех");
+                CaptchaSwitch();
+                failedAttempts = 0;
+            }
+        }
+
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy ||
+                e.Command == ApplicationCommands.Cut ||
+                e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+
+
         private void ButtonReg_Click(object sender, RoutedEventArgs e)
         {
+            NavigationService?.Navigate(new RegPage());
         }
-
         private void ButtonChangePassword_Click(object sender, RoutedEventArgs e)
         {
+            NavigationService?.Navigate(new ChangePassPage());
         }
-
-        private void TextBoxLogin_TextChanged(object sender, TextChangedEventArgs e)
-        {
-        }
-
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-        }
-
         private void txtHintLogin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             TextBoxLogin.Focus();
         }
-
         private void txtHintPass_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             PasswordBox.Focus();
